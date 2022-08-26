e260a6e76917088ef4ca3961295176d0be327b34
sandemoid <infosandemo@gmail.com>
2022-08-26 13:29:38 +0700

    p

--- sandi.sql
+++ sandi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 24, 2022 at 07:23 PM
+-- Generation Time: Aug 26, 2022 at 08:29 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -43,8 +43,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
-(1, 'Sandi Maulidika', 'sandimaulidika@gmail.com', 'default.jpg', '$2y$10$CEBIqmITMMQgsweLP9BNk.PScaA8QW5IhLctVQLft4tecN50yaJba', 1, 1, 1661189930),
-(2, 'Sandemo', 'infosandemo@gmail.com', 'default.jpg', '$2y$10$z2esOiqSxB1/I.xLfk1PNOPpnpQZIrYNb8Mhu3fmOz.ngo7.T33k.', 2, 1, 1661190412);
+(1, 'Sandi Maulidika', 'sandimaulidika@gmail.com', 'SanSan.jpg', '$2y$10$QNZyEGDDDa9CjIvG08Ny0OrSibp.RrF5ZjFLvu99oCeGu.04QXq7u', 1, 1, 1661189930);
 
 -- --------------------------------------------------------
 
@@ -111,7 +110,8 @@ CREATE TABLE `user_role` (
 
 INSERT INTO `user_role` (`id`, `role`) VALUES
 (1, 'Admin'),
-(2, 'Developer');
+(2, 'Developer'),
+(6, 'Member');
 
 -- --------------------------------------------------------
 
@@ -138,10 +138,32 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 (4, 3, 'Menu Management', 'menu', 'folder-17 text-danger', 1),
 (5, 3, 'Submenu Management', 'menu/submenu', 'ui-04 text-danger', 1),
 (8, 1, 'Role', 'admin/role', 'circle-08 text-info', 1),
-(10, 5, 'Setting', 'setting', 'settings text-primary', 1),
 (12, 1, 'User Management', 'admin/user', 'circle-08 text-success', 1),
-(15, 6, 'Blog', 'page', 'palette text-danger', 1),
-(16, 6, 'Portfolio', 'page/project', 'palette text-danger', 1);
+(17, 5, 'Setting', '#', 'settings-gear-65 text-primary', 1),
+(18, 6, 'Blog', '#', 'palette text-danger', 1),
+(19, 6, 'Portfolio', '#', 'palette text-danger', 1),
+(20, 6, 'About', '#', 'palette text-danger', 1),
+(21, 5, 'Testimoni', '#', 'settings-gear-65 text-primary', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_token`
+--
+
+CREATE TABLE `user_token` (
+  `id` int(11) NOT NULL,
+  `email` varchar(225) NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_token`
+--
+
+INSERT INTO `user_token` (`id`, `email`, `token`, `date_created`) VALUES
+(1, 'infosandemo@gmail.com', 'hHK0NnmeEuuRSVhksGI2lNanbtdHFo8CMNRfVc6xvRA=', 1661489975);
 
 --
 -- Indexes for dumped tables
@@ -178,6 +200,12 @@ ALTER TABLE `user_sub_menu`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user_token`
+--
+ALTER TABLE `user_token`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -185,7 +213,7 @@ ALTER TABLE `user_sub_menu`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user_access_menu`
@@ -203,13 +231,19 @@ ALTER TABLE `user_menu`
 -- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `user_token`
+--
+ALTER TABLE `user_token`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
